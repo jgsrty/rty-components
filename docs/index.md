a8e0b69c2d4ba2b80dc5844f0a94eae6142fc695
jgsrty <jgsrty@users.noreply.github.com>
2022-07-18 08:39:13 +0000

    deploy: c60cf61f9ae2034d176b426b7f15ed67cee04e56

--- docs/index.md
+++
@@ -1,9 +0,0 @@
-# test
-
-## testtest
-
-<script setup>
-import { xmdButton } from 'rty-components'
-</script>
-
-<xmd-button />
--- packages/components/src/button/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <button>test-btn</button>
-  </div>
-</template>
-
-<script setup></script>
-
-<style scoped></style>
